5f5dbac41b1030e7c5ecd0b3600f60e0156112af
Himanish <3hheman@gmail.com>
2024-04-12 18:01:49 -0400

    process requests in child process

--- own-server.c
+++ own-server.c
@@ -7,6 +7,23 @@
 
 #define PORT 3001
 
+void crequest(int conn) {
+  char buffer[1024] = { 0 };
+  ssize_t bytesRead = read(conn, buffer, 1024);
+  buffer[bytesRead] = '\0';
+  printf("Message \"%s\" received.\n", buffer);
+
+  if (strcmp(buffer, "quitc") == 0) {
+    char* message = "Thanks, I will quit.";
+    send(conn, message, strlen(message), 0);
+    printf("Message \"%s\" sent.\n", message);
+  } else {
+    char* message = "Message from server.";
+    send(conn, message, strlen(message), 0);
+    printf("Message \"%s\" sent.\n", message);
+  }
+}
+
 int main(int argc, char const* argv[]) {
   struct sockaddr_in address;
   int address_size = sizeof(address);
@@ -28,21 +45,29 @@ int main(int argc, char const* argv[]) {
     exit(EXIT_FAILURE);
   }
 
-  int conn = accept(socket_descriptor, (struct sockaddr*) &address, (socklen_t*) &address_size);
-  if (conn < 0) {
-    perror("accept failed");
-    exit(EXIT_FAILURE);
+  int requestsServed = 0;
+  while(requestsServed < 2) {
+    int conn = accept(socket_descriptor, (struct sockaddr*) &address, (socklen_t*) &address_size);
+    if (conn < 0) {
+      perror("accept failed");
+      exit(EXIT_FAILURE);
+    }
+
+    int pid = fork();
+    if (pid == 0) { // child
+      printf("in child\n");
+      close(l);
+      crequest(conn);
+      exit(0);
+      close(conn);
+    } else if (pid > 0) { // parent
+      printf("in parent\n");
+      close(conn);
+    }
+
+    requestsServed++;
   }
 
-  char buffer[1024] = { 0 };
-  read(conn, buffer, 1024);
-  printf("Message \"%s\" received.\n", buffer);
-
-  char* message = "Message from server.";
-  send(conn, message, strlen(message), 0);  
-  printf("Message \"%s\" sent.\n", message);
-
-  close(conn);
   shutdown(socket_descriptor, SHUT_RDWR);
 
   return 1;
